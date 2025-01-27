c9443e1cb1ce9995c878ec7f4060549a5f9dfb98
Elya <elyaj.le@gmail.com>
2025-01-27 14:34:33 -0600

    add amplify build configuration

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- commonground.site/frontend/src/routes/+page.svelte -->
+<script>
+  // Any JavaScript logic can be added here if needed
+</script>
+
+<style>
+  /* optional CSS styling */
+  h1 {
+    text-align: center;
+    margin-top: 20%;
+    font-size: 2rem;
+    color: #black; 
+  }
+  p {
+    text-align: center;
+    font-size: 1.2rem;
+    color: #333;
+    margin: 10px 0;
+  }
+</style>
+
+<main>
+  <h1>Welcome to Common Ground</h1>
+  <p>We built this for the lover girls, gays and theys of the world</p>
+</main>
